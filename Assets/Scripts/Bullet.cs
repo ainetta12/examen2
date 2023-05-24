19d523332d88caa5831f1939371ce61d39471b99
ainetta12 <ainasanzg@gmail.com>
2023-05-24 10:54:15 +0200

    examen

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Bullet : MonoBehaviour
+{
+
+    Rigidbody2D rBody2D;
+    public float bulletSpeed= 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody2D = GetComponent<Rigidbody2D>();
+        rBody2D.AddForce(transform.right * bulletSpeed,ForceMode2D.Impulse);
+    }
+
+  
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Destroy(this.gameObject);
+    }
+}
